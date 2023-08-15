8b4934fc0708206ea940efa076282198fe9684e6
Bernadette <bernadetteph05@gmail.com>
2023-08-14 22:10:34 -0400

    Insert Ordinary

--- Everyday_Sql/08_14_2023/SQLQuery1.sql
+++ Everyday_Sql/08_14_2023/SQLQuery1.sql
@@ -3,12 +3,12 @@
 
 
 insert into customer 
-select 1, 'Friedrich', 'Lacrete', 'lacrete.friedrich@hotmail.com', 5, 1, getdate(), getdate()
+select 1, 'Ordinary', 'Lacrete', 'lacrete.friedrich@hotmail.com', 5, 1, getdate(), getdate()
 where not exists (select *
 from customer
 where 
-first_name='Friedrich'and last_name='Lacrete'and email='lacrete.friedrich@hotmail.com' and address_id=5 and active=1)
+first_name='Ordinary'and last_name='Lacrete'and email='lacrete.friedrich@hotmail.com' and address_id=5 and active=1)
 
 select *
 from customer
-where first_name='Friedrich'
+where last_name='Lacrete'
