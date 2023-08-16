0ce74ffb5f19713b396202fed834a1b576b7f0c8
Bernadette <bernadetteph05@gmail.com>
2023-08-15 23:50:08 -0400

    Posted exo1

---
+++ Everyday_Sql/08_15_2023_homework/exo1.sql
@@ -0,0 +1,6 @@
+--1 From the following table write a query in SQL to concatenate the name, 
+--color, and productnumber columns.
+
+select CONCAT (name,+' '+ color, +' '+ProductNumber) [Concatenate_Info]
+from production.product
+order by productID
